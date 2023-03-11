9c0e78ce5c8693e40dd623c85d764ed159f8488b
jauntywunderkind <jaunty+wunder+kind+git@voodoowarez.com>
2023-03-11 14:02:44 -0500

    make run!

--- lua/random/init.lua
+++ lua/random/init.lua
@@ -1,4 +1,6 @@
-local colorschemesAll = vim.fn.getcompletion({ "", "color", 0 })
-local randomSchemeIndex = colorschemesAll[ math.random( #colorschemesAll ) ]
-print(randomSchemeIndex)
-print(colorschemesAll[randomSchemeIndex])
+local colorschemesAll = vim.fn.getcompletion("", "color")
+local randomScheme = colorschemesAll[ math.random( #colorschemesAll ) ]
+vim.cmd("colorscheme " .. randomScheme)
+-- todo: make this configurable
+-- ref: https://github.com/folke/tokyonight.nvim/blob/main/lua/tokyonight/config.lua
+print(randomScheme)
