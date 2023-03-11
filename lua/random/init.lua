869e5b62e767d030612c2f9949af5f95bf43346e
jauntywunderkind <jaunty+wunder+kind+git@voodoowarez.com>
2023-03-11 14:07:30 -0500

    convert to a fn & color

--- lua/random/init.lua
+++ lua/random/init.lua
@@ -1,6 +1,13 @@
-local colorschemesAll = vim.fn.getcompletion("", "color")
-local randomScheme = colorschemesAll[ math.random( #colorschemesAll ) ]
-vim.cmd("colorscheme " .. randomScheme)
--- todo: make this configurable
--- ref: https://github.com/folke/tokyonight.nvim/blob/main/lua/tokyonight/config.lua
-print(randomScheme)
+local M = {}
+
+function M.random()
+	local colorschemesAll = vim.fn.getcompletion("", "color")
+	local randomIndex = math.random( #colorschemesAll )
+	local randomScheme = colorschemesAll[ randomIndex ]
+	vim.cmd("colorscheme " .. randomScheme)
+	-- todo: make this configurable
+	-- ref: https://github.com/folke/tokyonight.nvim/blob/main/lua/tokyonight/config.lua
+	print(randomScheme)
+end
+
+return M
