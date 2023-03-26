2a217cbe9bd536917c742b8e35392428eca70242
jauntywunderkind <jaunty+wunder+kind+git@voodoowarez.com>
2023-03-25 23:11:19 -0400

    nice printing. better structure.

--- colors/random.lua
+++ colors/random.lua
@@ -1,3 +1,1 @@
-local scheme = require("random").random
--- if we try to execute this now, it doesn't do anything. but wrapped in defer_fn it works.
-vim.defer_fn(scheme, 0)
+require("random").random()
--- lua/random/init.lua
+++ lua/random/init.lua
@@ -1,14 +1,43 @@
 local M = {}
 
-function M.random()
+-- find a random color --- but don't set
+function M.pick()
 	local colorschemesAll = vim.fn.getcompletion("", "color")
 	local randomIndex = math.random( #colorschemesAll )
-	local randomScheme = colorschemesAll[ randomIndex ]
-	vim.cmd.colorscheme(randomScheme)
-	-- todo: make this configurable
+	local color = colorschemesAll[ randomIndex ]
+	return color
+end
+
+-- set a random color
+function M.choose(color)
+	color = color or M.pick()
+	-- the color doesn't work unless we defer
+	vim.defer_fn(function()
+		vim.cmd.colorscheme(color)
+	end, 0)
+	return color
+end
+
+-- print without making user hit enter key
+function M.print(color)
+	local oldHeight = vim.o.cmdheight
+	if oldHeight < 1 then
+		-- create space for the print to appear in
+		vim.o.cmdheight = 1
+		vim.cmd.redraw()
+		vim.defer_fn(function()
+			-- reset back eventually
+			vim.o.cmdheight = oldHeight
+		end, 6000)
+	end
+	print(color)
+end
+
+function M.random(color)
+	local color = color or M.choose()
+	-- todo: make whether we print configurable
 	-- ref: https://github.com/folke/tokyonight.nvim/blob/main/lua/tokyonight/config.lua
-	print(randomScheme)
-	return randomScheme
+	M.print(color)
 end
 
 return M
