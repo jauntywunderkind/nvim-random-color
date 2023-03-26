20b04e96f22e8cabcfb816a0683c92b5e76895b6
jauntywunderkind <jaunty+wunder+kind+git@voodoowarez.com>
2023-03-25 21:58:48 -0400

    defer_fn semi to the rescue

--- colors/random.lua
+++ colors/random.lua
@@ -1,2 +1,3 @@
-local scheme = require("random").random()
-vim.cmd.colorscheme(scheme)
+local scheme = require("random").random
+-- if we try to execute this now, it doesn't do anything. but wrapped in defer_fn it works.
+vim.defer_fn(scheme, 0)
